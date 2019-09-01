fd1394693f0f92af8e93c1b7e1d6facac563d471
Numan Karakaya <numan1820@hotmail.com>
2019-09-01 14:53:52 +0300

    chat details

--- routes/chat.js
+++ routes/chat.js
@@ -2,9 +2,8 @@ const express = require('express');
 const router = express.Router();
 
 /* GET chat page. */
-router.get('/', (req, res, next)=> {
-  console.log(req.user);
-  res.render('chat',{user:req.user});
-});
+router.get('/', (req, res, next) => {
+  res.render('chat', { user: req.user });
+  });
 
 module.exports = router;
--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,10 @@ const express = require('express');
 const router = express.Router();
 
 /* GET home page. */
-router.get('/', (req, res, next)=> {
-  res.render('index', { title: 'Express' });
+router.get('/', (req, res, next) => {
+	if (!req.user)
+		res.render('index', { title: 'Express' });
+	else
+		res.redirect('/chat');
 });
 module.exports = router;
